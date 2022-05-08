7d811cd321328b201061aa7472f5d6ff1f49727c
yuvischwartz <yuval.orderdesk@gmail.com>
2022-05-07 22:08:03 -0200

    added

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./lib/models')
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
 var app = express();
-
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
